1c69811b4c682e55082c2cc8df8f802db77b290d
Nischal Sharma <116507248+nischaldoescode@users.noreply.github.com>
2025-08-11 17:25:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ import { Application } from "oak/mod.ts";
 import { cacheRoutes } from "./utils/cache-routes.js";
 import { router } from "./routes/routes.js";
 
-const app1 = new Application();
-app1.use(cacheRoutes(3600));
-app1.use(router.routes());
-app1.use(router.allowedMethods());
+const app = new Application();
+app.use(cacheRoutes(3600));
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-console.log("Oak server running at http://localhost:4001");
-await app1.listen({ port: 4001 });
+export default app.handle;
