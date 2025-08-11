d3b9e26c4b7278a4b54f3020d87afd4b64b2768b
Nischal Sharma <116507248+nischaldoescode@users.noreply.github.com>
2025-08-11 17:33:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-import { Application } from "oak/mod.ts";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import { cacheRoutes } from "./utils/cache-routes.js";
 import { router } from "./routes/routes.js";
+import { compose } from "oak/middleware.ts";
 
-const app = new Application();
-app.use(cacheRoutes(3600));
-app.use(router.routes());
-app.use(router.allowedMethods());
+const composed = compose([
+  cacheRoutes(3600),
+  router.routes(),
+  router.allowedMethods(),
+]);
 
-export default app.handle;
+export default (req) => composed(req);
