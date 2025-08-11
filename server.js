33eca6119a5c7390243ec77755254943f5cbfd65
Nischal Sharma <116507248+nischaldoescode@users.noreply.github.com>
2025-08-11 21:04:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { cacheRoutes } from "./utils/cache-routes.js";
 import { router } from "./routes/routes.js";
-import { compose } from "oak/middleware.ts";
 
-const composed = compose([
-  cacheRoutes(3600),
-  router.routes(),
-  router.allowedMethods(),
-]);
+const app1 = new Application();
+app1.use(cacheRoutes(3600));
+app1.use(router.routes());
+app1.use(router.allowedMethods());
 
-export default (req) => composed(req);
+await app1.listen({ port: 4001 });
+console.log("Oak server running at http://localhost:4001");
