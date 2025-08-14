df5f1c5098e33091f1067f37a2b20adf4d0ac502
Nischal Sharma <116507248+nischaldoescode@users.noreply.github.com>
2025-08-15 00:50:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,13 @@ app1.use(cacheRoutes(3600));
 app1.use(router.routes());
 app1.use(router.allowedMethods());
 
-console.log("Oak server running at http://localhost:4001");
-await app1.listen({ port: 4001 });
+// Detect if running on Deno Deploy
+if (Deno.env.get("DENO_DEPLOYMENT_ID")) {
+  console.log("Running on Deno Deploy");
+  export default {
+    fetch: app1.fetch, // Oak handles requests via fetch API
+  };
+} else {
+  console.log("Oak server running at http://localhost:4001");
+  await app1.listen({ port: 4001 });
+}
